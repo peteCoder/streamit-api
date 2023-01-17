1d380bea7e632791ec2ae46d38e902df86f9c85b
peteCoder <petercodercoder@gmail.com>
2023-01-17 12:10:40 +0000

    added

--- streamit/settings.py
+++ streamit/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
