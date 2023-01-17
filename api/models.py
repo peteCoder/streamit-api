b6c4de06274c3a49aa166755d120d118c4c03bf7
peteCoder <petercodercoder@gmail.com>
2023-01-17 17:28:40 +0100

    committed

--- api/models.py
+++ api/models.py
@@ -39,12 +39,12 @@ class Video(models.Model):
         return self.author.email
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_profile(sender, instance, **kwargs):
+#     instance.profile.save()
