739e0c77a785796fc2735f1fe83cb2daab5a168e
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-01-19 17:17:55 +0100

    added change password functionality

--- client/client.py
+++ client/client.py
@@ -1,18 +1,15 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:8000/users/change-password/'
+# url = 'http://127.0.0.1:8000/users/change-password/'
 # url = 'http://127.0.0.1:8000/users/password_reset/confirm/'
-
+url = "http://127.0.0.1:8000/api/videos/"
 
 payload = {'old_password': 'petertalk', 'new_password': 'propensity'}
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Token afbf91b454b50cc56637d34b77133fef3c6604da'
 }
-r = requests.put(url, data=json.dumps(payload), headers=headers)
+r = requests.get(url, headers=headers)
 
 print(r.json())
-# if r.status_code == 200:
-#     print(r.json())
-    
