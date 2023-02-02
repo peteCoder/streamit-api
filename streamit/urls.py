9703028a88e34c40090771d0621ce5fcb1127186
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-02-02 10:45:25 +0100

    customise admin dashboard headers

--- streamit/urls.py
+++ streamit/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
+admin.site.site_header  =  "TSL Nigeria Administration"  
+admin.site.site_title  =  "TSL Nigeria"
+admin.site.index_title  =  "DASHBOARD"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
