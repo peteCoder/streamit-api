af83440272ed72980d7283b6c892ed2cca3cfb1c
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-02-06 14:49:34 +0100

    email sending functionality

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('auth-token/', views.CustomAuthToken.as_view(), name="user-token"),
     # path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     # path('change-password/', views.ChangePasswordView.as_view(), name='change-password'),
+    
     # Djoser
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')), 
