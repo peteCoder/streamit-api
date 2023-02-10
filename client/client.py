cfc5bdb8b2cdffdc8447f3cc81f96bed52ec2858
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-02-10 10:17:34 +0100

    committed

--- client/client.py
+++ client/client.py
@@ -1,33 +1,13 @@
 import requests
-import json
 
-# curl -X POST -d "client_id=1OZlPWZurfIzJ0Lx905DUcYqWrxlaIwUizfKxzId&client_secret=TSAmKVn8ZVcqIiAOxeC6OOvVPzw2AkNbIOQJe5BhWeU45h8hcmFe0MS9Nu6cG5IAGyh7S6st655bBHOt28m2AanzoUez7xwTPyRzegTEY4ANhltZYrbfVgrMVs5m5Cla&grant_type=password&username=talk2peteresezobor@gmail.com&password=propensity" http://localhost:8000/auth/token
-# {'token': '88b1a2687fc3ff1e3b62ecc670560c5c6fef8d2f', 'user_id': 2, 'email': 'tony@gmail.com'}
-# url = 'http://127.0.0.1:8000/api/actor/'
-# # url = 'http://127.0.0.1:8000/api/user/auth-token/'
-
-# payload = {'client_id': 'T0LkP3v4vKiwGDYWFP0ihfHUyf5E6GSqs0IGuxUS', 'client_secret':'pbkdf2_sha256$260000$3VD4PEwI7wfgYFwX0l6SlX$nHpnXLf7IMEorvgQXk7TVuLAOnceWUaznzlwgzRsirY', '​grant_type':'​password', 'username':'petertalk@gmail.com', 'password':'propensity'}
-# headers = {
-#     'Content-Type': 'application/json',
-#     'Authorization': 'Token 88b1a2687fc3ff1e3b62ecc670560c5c6fef8d2f'
-# }
-# r = requests.get(url, data=payload, headers=headers)
-
-# print(r.json())
-
-# import requests
-
-url = "http://127.0.0.1:8000/oauth/token/"
+url = "https://web-production-93c3.up.railway.app/oauth/convert-token/"
 
 payload = {
-    "client_id": r"T0LkP3v4vKiwGDYWFP0ihfHUyf5E6GSqs0IGuxUS",
-    "client_secret": r"pbkdf2_sha256$260000$3VD4PEwI7wfgYFwX0l6SlX$nHpnXLf7IMEorvgQXk7TVuLAOnceWUaznzlwgzRsirY=",
-    "grant_type": r"password",
-    "username": r"talk2peteresezobor@gmail.com",
-    "password": r"propensity"
+    "query": "{\"client_id\":\"ceDPTrU2KFFiisy3xwLYxw0kMH9fCZTfU1uPJTq2\",\"client_secret\":\"VdrBI0PKl990YADF5CA9QypWeOoS0tPWoXqTX6B06jM4Vwguz5IJVSQuNseruo1LuKNKltRqWAypFNim020dXF9QMKj08gv2BiDrBlkjrwJ1NquQhkHY9atOMjFaRJsv\", \"backend\":\"google-oauth2\", \"token\":\"039516402443-522feng3uud7h8gvc936133biv5s4k61.apps.googleusercontent.com\", \"grant_type\":\"convert_token\"}",
+    "variables": {}
 }
 headers = {"Content-Type": "application/json"}
 
 response = requests.request("POST", url, json=payload, headers=headers)
 
-print(response.json())
+print(response.text)
