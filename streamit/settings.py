70f0fc86532bf85ce88db12063e7ec9959c6a45f
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-02-13 08:22:13 +0100

    committed database

--- streamit/settings.py
+++ streamit/settings.py
@@ -114,24 +114,24 @@ WSGI_APPLICATION = 'streamit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'D4imfMoVODfTck00VEWD',
-        'HOST': 'containers-us-west-43.railway.app',
-        'PORT': '6946',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'D4imfMoVODfTck00VEWD',
+#         'HOST': 'containers-us-west-43.railway.app',
+#         'PORT': '6946',
+#     }
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True
 
